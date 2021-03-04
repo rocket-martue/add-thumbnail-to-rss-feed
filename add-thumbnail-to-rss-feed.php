2c39b09b9a75c55be8ceae298dde6c0aa48db255
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2021-03-04 17:09:18 +0900

    Update add-thumbnail-to-rss-feed.php

--- add-thumbnail-to-rss-feed.php
+++ add-thumbnail-to-rss-feed.php
@@ -11,11 +11,17 @@
  */
 
 add_action (
+	'rss2_ns',
+	function() {
+		echo 'xmlns:media="http://search.yahoo.com/mrss/"';
+	}
+);
+add_action (
 	'rss2_item',
 	function() {
 		global $post;
 		if ( has_post_thumbnail( $post->ID ) ) {
-			$thumbnail = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ), 'large' );
+			$thumbnail = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
 			echo "<media:thumbnail>" .$thumbnail. "</media:thumbnail>";
 		}
 	}
