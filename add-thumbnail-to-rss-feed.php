9906a4b365758b679448bb08dac5492fe0fd2bed
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2021-03-04 09:29:44 +0900

    Create add-thumbnail-to-rss-feed.php

---
+++ add-thumbnail-to-rss-feed.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin name: Add thumbnail to rss feed
+ * Plugin URI:
+ * Description: Add thumbnail to rss feed.
+ * Version: 1.0.0
+ * Author: Rocket Martue
+ * Created: Mar. 4, 2021
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+add_action (
+	'rss2_item',
+	function() {
+		global $post;
+		if ( has_post_thumbnail( $post->ID ) ) {
+			$thumbnail = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ), 'large' );
+			echo "<media:thumbnail>" .$thumbnail. "</media:thumbnail>";
+		}
+	}
+);
