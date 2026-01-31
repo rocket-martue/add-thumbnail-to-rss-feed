891b2f456288c4da772b4de20e299c75b845f257
Toshiyuki Honda <rocket.martue@gmail.com>
2026-01-31 12:15:50 +0900

    WordPress Coding Standards準拠＆セキュリティ確認済み: サムネイルをRSSに追加

--- add-thumbnail-to-rss-feed.php
+++ add-thumbnail-to-rss-feed.php
@@ -1,40 +1,50 @@
 <?php
 /**
- * Plugin name: Add thumbnail to rss feed
- * Plugin URI: https://github.com/rocket-martue/add-thumbnail-to-rss-feed
- * Description: Add thumbnail to rss feed.
- * Version: 1.0.1
- * Author: Rocket Martue
- * Created: Mar. 4, 2021
- * License: GPLv2 or later
+ * Plugin Name: Add Thumbnail to RSS Feed
+ * Plugin URI:  https://github.com/rocket-martue/add-thumbnail-to-rss-feed
+ * Description: Add featured image (thumbnail) to RSS feed.
+ * Version:     1.0.2
+ * Author:      Rocket Martue
+ * Created:     Mar. 4, 2021
+ * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package AddThumbnailToRSSFeed
  */
 
 /**
- * Add thumbnail to RSS feed.
+ * Add the media namespace to the rss2 root element.
  *
- * @param string $content The content of the RSS feed.
- * @return string The modified content of the RSS feed.
+ * @return void
  */
 add_action(
 	'rss2_ns',
-	function () {
-		echo 'xmlns:media="http://search.yahoo.com/mrss/"';
+	static function () {
+		echo ' xmlns:media="http://search.yahoo.com/mrss/"'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 );
 
 /**
- * Add thumbnail to RSS feed items.
+ * Output post thumbnail (featured image) in RSS <item> as <media:thumbnail>.
+ *
+ * @return void
  */
 add_action(
 	'rss2_item',
-	function () {
-		global $post;
-		if ( has_post_thumbnail( $post->ID ) ) {
-			$thumbnail = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ), 'full' );
-			echo '<media:thumbnail>' . esc_url( $thumbnail ) . '</media:thumbnail>';
+	static function () {
+		$post_id = get_the_ID();
+		if ( ! $post_id ) {
+			return;
+		}
+
+		if ( has_post_thumbnail( $post_id ) ) {
+			$thumbnail_url = wp_get_attachment_image_url( get_post_thumbnail_id( $post_id ), 'full' );
+			if ( $thumbnail_url ) {
+				printf(
+					'<media:thumbnail url="%s" />',
+					esc_url( $thumbnail_url )
+				);
+			}
 		}
 	}
 );
